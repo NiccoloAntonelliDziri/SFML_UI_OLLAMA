d0132b4b9fbde74daa80a4905cec06e433bc4e4f
Niccolo Antonelli Dziri <niccolo.dz.antonelli@gmail.com>
2025-01-05 17:26:36 +0100

    Is ready atomic bool to check if thread finished function call

--- src/thread_manager.hpp
+++ src/thread_manager.hpp
@@ -7,7 +7,9 @@
 template <typename ResultType>
 class ThreadManager {
     public:
-    ThreadManager() { this->future = this->promise.get_future(); }
+    ThreadManager() : isResultReady(false) {
+        this->future = this->promise.get_future();
+    }
     ~ThreadManager() { this->thread.join(); }
 
     // Pass function and arguments to start thread
@@ -15,13 +17,14 @@ class ThreadManager {
     // with type ResultType (class template parameter)
     template <typename F, typename... Args>
     void start(F &&f, Args &&...args) {
-        std::cout << "Type of f: " << typeid(f).name() << std::endl;
+        this->isResultReady = false;
 
         // If return type is void, different lambda function
         if constexpr (std::is_same_v<ResultType, void>) {
             this->thread = std::thread([this, f, args...]() {
                 f(args...);
                 this->promise.set_value();
+                this->isResultReady = true;
             });
         } else {
             // Initialize thread with lambda function
@@ -33,8 +36,11 @@ class ThreadManager {
     // Get result from promise
     ResultType getResult() { return this->future.get(); }
 
+    bool isReady() { return this->isResultReady; }
+
     private:
     std::thread thread;
     std::promise<ResultType> promise;
     std::future<ResultType> future;
+    std::atomic<bool> isResultReady;
 };
